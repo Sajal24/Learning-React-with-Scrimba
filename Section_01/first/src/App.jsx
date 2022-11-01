6cd16ae0bd36382847299fef5d1895ced8913334
Sajal Sharma <sajalsharma2408@gmail.com>
2022-11-01 14:04:48 +0530

    react with vite

---
+++ Section_01/first/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import analaddicts from "./analaddicts.jpg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a
+          href="https://www.pinterest.ca/pin/795940934158310294/"
+          target="_blank"
+        >
+          <img
+            src={analaddicts}
+            className="logo react"
+            alt="React logo"
+            width="200px"
+            height="500px"
+          />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
